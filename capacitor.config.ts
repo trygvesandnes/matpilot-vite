f6e82a03f3c88f8b534daa60115e7b8febda5940
trygvesandnes <t_sandnes@hotmail.com>
2026-06-21 21:43:39 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,1 +1,15 @@
+import { CapacitorConfig } from '@capacitor/cli';
 
+const config: CapacitorConfig = {
+  appId: 'no.matpilot.app',
+  appName: 'Matpilot',
+  webDir: 'dist',
+  ios: {
+    contentInset: 'automatic',
+  },
+  server: {
+    androidScheme: 'https',
+  },
+};
+
+export default config;
