8cd7873f0e54adacfc9227638b49a49521d4584a
trygvesandnes <t_sandnes@hotmail.com>
2026-06-22 22:57:22 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'no.matpilot.app',
-  appName: 'Matpilot',
+  appName: 'Matsmart',
   webDir: 'dist',
   ios: {
     contentInset: 'automatic',
